76ad5e0f80d47ef3f9fef1cfbbeae3e3f2be54e5
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-27 11:27:59 -0600

    add trips db table and seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+85.times do
+  Trip.create!(duration: 63, start_date: '8/29/2013 14:13', start_station: 66, end_date: '8/29/2013 14:14', end_station: 66, bike_id: 520, subscription_type: 'Subscriber', zip_code: 94127)
+end
