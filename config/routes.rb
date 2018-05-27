a247912b22c5ddc2f762b53dad7558c7ab59519f
Jliv316 <jclivingston316@gmail.com>
2018-05-27 15:02:06 -0600

    adds stations controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:show]
+  resources :stations, only: [:index]
 end
---
+++ spec/features/stations/user_can_see_all_stations_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'visitor' do
+  context 'visits stations index page' do
+    it 'should see a list of stations with all attributes' do
+      
+      visit '/stations'
+
+      station1 = Station.create(name: 'Fort Collins Downtown', dock_count: 5, city: "Fort Collins", installation_date: "01/18/2015")
+      station2 = Station.create(name: 'Blake St', dock_count: 10, city: "Denver", installation_date: "02/22/2013")
+      station3 = Station.create(name: 'Larimer St', dock_count: 2, city: "Denver", installation_date: "07/18/2018")
+
+      expect(page).to have_content(station1.name)
+      expect(page).to have_content(station2.name)
+      expect(page).to have_content(station3.name)
+      expect(page).to have_content(station1.dock_count)
+      expect(page).to have_content(station2.dock_count)
+      expect(page).to have_content(station3.dock_count)
+      expect(page).to have_content(station1.city)
+      expect(page).to have_content(station2.city)
+      expect(page).to have_content(station3.city)
+      expect(page).to have_content(station1.installation_date)
+      expect(page).to have_content(station2.installation_date)
+      expect(page).to have_content(station3.installation_date)
+    end
+  end
+end
