f92e468c9dbb790f13f2484cdfee4e154b19b1dd
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-27 19:43:54 -0600

    TEST: Working on scoping to add a specific item to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,11 @@
 class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def create
     accessory = Accessory.find(params[:id])
-    flash[:notice] = "Added #{accessory.title} to your cart"
+    session[:cart] ||= Hash.new(0)
+    session[:cart][accessory.id.to_s] = session[:cart][accessory.id.to_s] + 1
+    flash[:notice] = "Added #{pluralize(session[:cart][accessory.id.to_s], accessory.title)} to your cart."
     redirect_to request.referer
   end
 end
--- spec/cart/user_can_add_accessories_to_their_cart_spec.rb
+++ spec/cart/user_can_add_accessories_to_their_cart_spec.rb
@@ -3,39 +3,39 @@ require 'rails_helper'
 describe 'Visitor index page' do
   it 'allows user to add an accessory to their cart' do
 
-    accessory = create(:accessory)
+    accessory = create(:accessory, title: "Pizza Cat 13")
 
     visit '/bike-shop'
 
     within first("div.accessories") do
       click_on "Add to Cart"
     end
-
-    expect(page).to have_content("Added #{accessory.title} to your cart")
+# save_and_open_page
+    expect(page).to have_content("Added 1 #{accessory.title} to your cart")
   end
 end
 
 describe 'Visitor index page' do
   it 'displays the the increment amount of accessories' do
 
-    accessory_1 = create(:accessory, title: "Pizza Cat 1")
+    accessory_1 = create(:accessory, title: "Pizza Cat 1", id: 1)
 
     visit '/bike-shop'
 
-    within first("div.accessories") do
+    # within first("div.accessories") do
       click_on "Add to Cart"
-    end
+    # end
 
-    expect(page).to have_content("Added 1 #{accessory_1.title} in your cart.")
+    expect(page).to have_content("Added 1 #{accessory_1.title} to your cart.")
 
-    accessory_2 = create(:accessory, title: "Pizza Cat 2")
+    accessory_2 = create(:accessory, title: "Pizza Cat 2", id: 2)
 
     visit '/bike-shop'
 
-    within last("div.accessories") do
+    # within find_by_id(2) do
       click_on "Add to Cart"
-    end
+    # end
 
-    expect(page).to have_content("Added 2 #{accessory_2.title} in your cart.")
+    expect(page).to have_content("Added 2 #{accessory_2.title} to your cart.")
   end
 end
