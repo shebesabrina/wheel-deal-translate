a9b0f62813c90ed4c06e9bbf301ada464ff6af10
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-28 16:58:45 -0600

    drop,create,migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_05_28_211435) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_211435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "trips", force: :cascade do |t|
     t.integer "duration"
     t.text "start_date"
