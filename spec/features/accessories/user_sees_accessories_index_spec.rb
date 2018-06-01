b9f33aba87fc82f7b670c400715af07885660e92
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-31 21:37:32 -0600

    Test - image is not testable at this time

--- spec/features/accessories/user_sees_accessories_index_spec.rb
+++ spec/features/accessories/user_sees_accessories_index_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe 'Visitor show page' do
-  xit 'displays the information of one accessory' do
+describe 'current_user Index page' do
+  it 'displays the information of one accessory' do
     accessory = create(:accessory)
 
     visit accessory_path(accessory)
 
-    expect(page).to have_content(accessory.thumbnail)
+    # expect(page).to have_content(accessory.thumbnail)
     expect(page).to have_content(accessory.title)
     expect(page).to have_content(accessory.description)
     expect(page).to have_content(accessory.price)
