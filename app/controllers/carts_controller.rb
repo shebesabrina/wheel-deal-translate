854fc789f54fdae985ae42d7a4c5491416ff6b50
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-04 01:24:51 -0600

    Quantity increment works on cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -22,13 +22,36 @@ class CartsController < ApplicationController
 
     redirect_to cart_path
   end
+  #
+  # def update
+  #   @contents.update(contents_pararms)
+  #   redirect_to request.referrer
+  # end
 
   def update
-    @contents.update(contents_pararms)
-    redirect_to request.referrer
+    accessory = Accessory.find(params[:item_id])
+    @cart = Cart.new(session[:cart])
+    if params[:decrease]
+      if @cart.contents[accessory.id.to_s] <= 1
+        delete_accessory(accessory.id.to_s)
+        @cart = Cart.new(session[:cart])
+      else
+        @cart.reduce_item_cart(accessory.id)
+      end
+    else
+      @cart.add_item_cart(accessory.id)
+    end
+    session[:cart] = @cart.contents
+    redirect_to cart_path
   end
 
   private
+
+def delete_accessory(id)
+  accessory = Accessory.find(id)
+  session[:cart].delete(id)
+  flash[:notice] = "Successfully removed #{view_context.link_to accessory.title, accessory_path(accessory)} from your cart"
+end
 
   def contents_pararms
     params.require(:accessory).permit(:price)
