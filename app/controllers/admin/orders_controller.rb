da86e7bf8eef8ca173bf53bd369f0f1af0e5c4ec
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-06 11:54:29 -0600

    Order update completed

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -25,4 +25,20 @@ class Admin::OrdersController < Admin::BaseController
   def status
     redirect_to :index
   end
+
+
+  def update
+    params["order"]["status"] = (params["order"]["status"])
+    order = Order.find(params[:id])
+    if order.update(order_params)
+      flash[:success] = "You have updated the order to #{params["order"]["status"]}!"
+    end
+    redirect_to admin_dashboard_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
