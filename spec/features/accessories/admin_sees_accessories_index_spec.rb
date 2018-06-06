590c0625ec761652dd21d348ffc65d59f6c89c36
Sabrina Robinson <sabrinarob@gmail.com>
2018-06-05 19:57:09 -0600

    Updated admkin index accessory test with correct route

--- spec/features/accessories/admin_sees_accessories_index_spec.rb
+++ spec/features/accessories/admin_sees_accessories_index_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
-describe 'User visits accessories index page' do
+describe 'Admin accessories index page' do
   context 'as admin' do
-    xit 'allows admin to see all accessories' do
-	    admin = User.create(username: 'penelope',
-                          password: 'boom',
-                          role: 1)
+    it 'allows admin to see all accessories' do
 
+	    admin = create(:user, role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit admin_bike_shop_accessories_path
+      accessory = create(:accessory)
+
+      visit admin_bike_shop_path
 
       expect(page).to have_content('Admin Accessories')
+      expect(page).to have_content(accessory.title)
+      expect(page).to have_content(accessory.description)
+      # expect(page).to have_content(accessory.thumbnail)
+      expect(page).to have_content('Edit')
+      expect(page).to have_button('Retire')
     end
   end
 end
