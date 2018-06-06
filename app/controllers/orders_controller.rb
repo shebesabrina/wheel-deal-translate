e2d4b02d765428d3ff8b421e6790577a59696f4f
Jliv316 <jclivingston316@gmail.com>
2018-06-05 21:15:56 -0600

    adds flash message to nav bar

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.create(user_id: current_user.id)
     @order.assign_accessories(@cart.contents)
+    flash[:notice] = "Successfully submitted your order totaling: $#{@order.total_price}"
     redirect_to '/dashboard'
   end
 
