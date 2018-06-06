76ea3db0cabc34261ffa7cfc8543cd1c1fafd6d5
Jliv316 <jclivingston316@gmail.com>
2018-06-05 21:19:18 -0600

    adds columns fullnamne and address to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_205428) do
+ActiveRecord::Schema.define(version: 2018_06_06_031710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2018_06_05_205428) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "address"
   end
 
   add_foreign_key "accessory_orders", "accessories"
